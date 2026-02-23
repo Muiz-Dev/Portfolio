0d7abee2c4da743715e10008c8fa70c8ec08c558
Muiz-Dev <muiz.dev.io@gmail.com>
2026-02-23 11:00:44 +0100

    Update robots.ts to use dynamic host URL for sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,12 +2,14 @@ import type { MetadataRoute } from 'next';
 import { absoluteUrl, siteUrl } from '@/lib/seo';
 
 export default function robots(): MetadataRoute.Robots {
+  const host = new URL(siteUrl).host;
+
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
     sitemap: absoluteUrl('/sitemap.xml'),
-    host: siteUrl,
+    host,
   };
 }
